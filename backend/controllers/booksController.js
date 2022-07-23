e25e6a43557c0823ec1c1804a3b297d3f90dd28f
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-07-23 16:52:07 -0400

    createBook controller done.

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -1,14 +1,14 @@
 import Book from '../models/bookModel.js'
 import mongoose from 'mongoose'
 
-// GET all books
+/* ----- GET all books -----*/
 export const getBooks = async (req, res) => {
   const books = await Book.find({}).sort({ createdAt: -1 })
 
   res.status(200).json(books)
 }
 
-// GET a book
+/* ----- GET a book -----*/
 export const getBook = async (req, res) => {
   const { id } = req.params
 
@@ -18,7 +18,42 @@ export const getBook = async (req, res) => {
 
   const book = await Book.findById(id)
 
+  // check if such book exists
   if (!book) return res.status(400).json({ error: 'No such workout exists!' })
 
   res.status(200).json(book)
+}
+
+/* ----- CREATE a book -----*/
+export const createBook = async (req, res) => {
+  // destructure from request body
+  const { title, author, publishYear } = req.body
+
+  // setup empty array to push empty fields
+  const emptyFields = []
+
+  // check if required body fields contain data and if not push to emptyFields array
+  if (!title) emptyFields.push('title')
+  if (!author) emptyFields.push('author')
+  if (!publishYear) emptyFields.push('publishYear')
+
+  // if emptyFields array contains elements, return error with missing fields
+  if (emptyFields)
+    return res
+      .status(400)
+      .json({ error: 'Please fill in all required fields!', emptyFields })
+
+  // try/catch to create and add doc to database
+  try {
+    const book = await Book.create({
+      title,
+      author,
+      publishYear,
+    })
+
+    // send back book as response if successful
+    res.status(200).json(book)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
 }
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,5 +1,9 @@
 import { Router } from 'express'
-import { getBooks } from '../controllers/booksController.js'
+import {
+  getBooks,
+  getBook,
+  createBook,
+} from '../controllers/booksController.js'
 
 export const router = Router()
 
@@ -7,14 +11,10 @@ export const router = Router()
 router.get('/', getBooks)
 
 // GET a single book
-router.get('/:id', (req, res) => {
-  res.status(200).send(req.params)
-})
+router.get('/:id', getBook)
 
 // POST a new book
-router.post('/', (req, res) => {
-  res.status(200).send('POST new book')
-})
+router.post('/', createBook)
 
 // DELETE a book
 router.delete('/:id', (req, res) => {
