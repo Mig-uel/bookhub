41fe0cd6ac0dfd89470ee134f364369a22db2337
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-07-23 16:32:27 -0400

    GET single book controller done.

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -7,3 +7,18 @@ export const getBooks = async (req, res) => {
 
   res.status(200).json(books)
 }
+
+// GET a book
+export const getBook = async (req, res) => {
+  const { id } = req.params
+
+  // check for invalid ID
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ error: 'Invalid book ID!' })
+
+  const book = await Book.findById(id)
+
+  if (!book) return res.status(400).json({ error: 'No such workout exists!' })
+
+  res.status(200).json(book)
+}
